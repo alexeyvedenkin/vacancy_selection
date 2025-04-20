a05884698b42e6444dbb0ee14752feb72281b225
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-04-20 18:08:02 +0500

    fixed user_interaction в src/utils

--- src/vacancies.py
+++ src/vacancies.py
@@ -10,21 +10,32 @@ class Vacancy:
         self.town = data.get('area', {}).get('name', '')
 
         # Adjust handling of salary_range to avoid NoneType errors
-        salary_range = data.get('salary_range')  # Get salary_range directly
-        if isinstance(salary_range, dict):  # Ensure it's a dictionary
-            self.salary_from = salary_range.get('from', 0) if isinstance(salary_range.get('from'), (int, float)) else 0
-            self.salary_to = salary_range.get('to', 0) if isinstance(salary_range.get('to'), (int, float)) else 0
+        salary = data.get('salary')  # Get salary_range directly
+        if isinstance(salary, dict):  # Ensure it's a dictionary
+            self.salary_from = salary.get('from', 0) if isinstance(salary.get('from'), (int, float)) else 0
+            self.salary_to = salary.get('to', 0) if isinstance(salary.get('to'), (int, float)) else 0
         else:
             self.salary_from = 0  # Default if salary_range is None or not a dict
             self.salary_to = 0  # Default if salary_range is None or not a dict
 
-        self.requirement = (data.get('snippet', {}).get('requirement', 'Требования не указаны')
-                            .replace('<highlighttext>', '').replace('</highlighttext>', ''))
+        description = data.get('snippet', {}).get('description', 'Описание не указано')
+        if description is None:
+            description = 'Описание не указано'
+        self.description = (description.replace('<highlighttext>', '')
+                            .replace('</highlighttext>', ''))
+
+        self.alternate_url = data.get('alternate_url')
+
+        requirement = data.get('snippet', {}).get('requirement', 'Требования не указаны')
+        if requirement is None:  # Check if requirement is None
+            requirement = 'Требования не указаны'  # Default message if None
+        self.requirement = (requirement.replace('<highlighttext>', '')
+                            .replace('</highlighttext>', ''))
         self.alternate_url = data.get('alternate_url')
 
     def __str__(self):
         return (f'''{self.name}, зарплата от {self.salary_from}, {self.town}, {self.alternate_url}, 
-        требования к вакансии: {self.requirement}\n''')
+        описание вакансии: {self.description}\n, требования к вакансии: {self.requirement}\n\n''')
 
     def to_dict(self):
         """ Converts the Vacancy instance to a dictionary for JSON serialization. """
@@ -47,7 +58,7 @@ class Vacancy:
         vacancies_list = []  # Формируем список вакансий, удовлетворяющих требованиям
         for data in data_list:
             if isinstance(data, dict):
-                vacancy_id = data.get('alternate_url')
+                vacancy_id = data.get('id')
                 if vacancy_id not in unique_vacancies:
                     unique_vacancies[vacancy_id] = cls(data)
                     vacancies_list.append(unique_vacancies[vacancy_id])
@@ -56,7 +67,8 @@ class Vacancy:
 
 if __name__ == "__main__":
     hh_api = HeadHunterAPI('data/vacancies.json')
-    hh_api.load_vacancies("машинист")
+    keyword = input('Введите поисковый запрос :')
+    hh_api.load_vacancies(keyword)
     for vacancy in hh_api.vacancies:
         print(vacancy)
 
