e80531ebcabe0278fb4b10cbf319f02d0cfa22d4
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-04-20 19:16:02 +0500

    добавлен метод add_to_zip в классе JSONWorker

--- src/file_worker.py
+++ src/file_worker.py
@@ -1,5 +1,6 @@
 import json
 import os
+import zipfile
 from abc import ABC, abstractmethod
 
 from config import DATA_DIR
@@ -28,3 +29,15 @@ class JSONWorker(BaseWorker):
         file_path = os.path.join(DATA_DIR, f"{self.filename}.json")  # Определяем полный путь к файлу
         with open(file_path, 'w', encoding='utf-8') as file:  # Открываем файл для записи
             json.dump(data_to_save, file, ensure_ascii=False)  # Записываем данные в формате JSON
+
+    def add_to_zip(self, zip_filename):
+        """ Метод для добавления JSON-файла в ZIP-архив """
+        json_file_path = os.path.join(DATA_DIR, f"{self.filename}.json")  # Путь к JSON-файлу
+
+        # Проверяем, существует ли файл перед добавлением в ZIP
+        if os.path.exists(json_file_path):
+            with zipfile.ZipFile(zip_filename, 'a') as zip_file:  # 'a' для добавления в существующий архив
+                zip_file.write(json_file_path, arcname=f"{self.filename}.json")  # Добавляем файл в архив
+                print(f'Файл {json_file_path} добавлен в архив {zip_filename}.')
+        else:
+            print(f'Файл не найден: {json_file_path}')
